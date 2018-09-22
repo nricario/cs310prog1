44f867136bd5fa80c6af4c0192df482737b08ca6
Nick <nick@ricario.com>
2018-09-21 22:44:22 -0700

    main not working (syntax)

--- src/main.java
+++ src/main.java
@@ -1,4 +1,13 @@
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.List;
+
+import javafx.scene.shape.Path;
 
 public class main {
+	List<String> lines = Files.readAllLines(Path p, Charset.defaultCharset());
+}
 
+void writeToFile( Path location, List<String> toWrite ){
+Files.write(location,toWrite,Charset.defaultCharset());
 }
