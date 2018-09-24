3d3ccede045cab47dd28d887dec5679ff965264d
Nick <nick@ricario.com>
2018-09-23 20:32:48 -0700

    first 4 methods (commit)

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import javafx.scene.shape.Path;
 
@@ -25,16 +26,52 @@ public class main {
 			String line = lines.get(i);
 			length += line.length();
 		}
-		avg = length/(lines.size());
+		avg = length / (lines.size());
 		return avg;
 	}
 
 	public int uniqueTokenInsen() {
-
+		List<String> tokens;
+		for (int i = 0; i < lines.size(); i++) {
+			StringTokenizer st = new StringTokenizer(lines.get(i));
+			while (st.hasMoreTokens()) {
+				boolean unique = true;
+				String pointer = st.nextToken();
+				for (int j = 0; j < tokens.size(); j++) {
+					String db = tokens.get(j);
+					if (pointer.equalsIgnoreCase(db)) {
+						unique = false;
+					}
+				}
+				if(unique == true)
+				{
+					tokens.add(pointer);
+				}
+			}
+		}
+		return tokens.size();
 	}
 
 	public int uniqueTokenSen() {
-
+		List<String> tokens;
+		for (int i = 0; i < lines.size(); i++) {
+			StringTokenizer st = new StringTokenizer(lines.get(i));
+			while (st.hasMoreTokens()) {
+				boolean unique = true;
+				String pointer = st.nextToken();
+				for (int j = 0; j < tokens.size(); j++) {
+					String db = tokens.get(j);
+					if (pointer.equals(db)) {
+						unique = false;
+					}
+				}
+				if(unique == true)
+				{
+					tokens.add(pointer);
+				}
+			}
+		}
+		return tokens.size();
 	}
 
 	public int numToken() {
