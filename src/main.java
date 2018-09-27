57cc3c1db8690226609fd61ab3f54fa794ac059e
Nick <nick@ricario.com>
2018-09-27 10:31:16 -0700

    implemented your 2 methods, make sure to look at your code, some mistakes but made some improvements

--- src/main.java
+++ src/main.java
@@ -2,6 +2,8 @@ import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import javafx.scene.shape.Path;
 
@@ -43,8 +45,7 @@ public class main {
 						unique = false;
 					}
 				}
-				if(unique == true)
-				{
+				if (unique == true) {
 					tokens.add(pointer);
 				}
 			}
@@ -65,8 +66,7 @@ public class main {
 						unique = false;
 					}
 				}
-				if(unique == true)
-				{
+				if (unique == true) {
 					tokens.add(pointer);
 				}
 			}
@@ -78,11 +78,52 @@ public class main {
 
 	}
 
-	public String freqToken() {
-
+	public String freqToken(List<String> lines, List<String> tokens) {
+		int maxOccuringCharacter;
+		for (int element : lines.) {
+			if (lines.get(element) == lines.get(maxOccuringCharacter))
+				;
+			tokens.add(lines.get(element));
+		}
+		for (int value = 0; value < lines.size(); value++) {
+			String line = lines.get(value);
+			String actualQuantity = "";
+			int actualFreqCount;
+			for (int item = 0; item < line.length(); item++) {
+				int tempFreqCount;
+				String tempElement = lines.get(item);
+				for (int element; element < line.length(); element++)
+					if (lines.get(element).equals(tempElement))
+						tempFreqCount++;
+				if (tempFreqCount >= actualFreqCount) {
+					actualQuantity = tempElement;
+					actualFreqCount = tempFreqCount;
+				}
+			}
+			System.out.println(actualFreqCount);
+		}
 	}
 
-	public int countFreqTokenInsen() {
+	public String freqTokenInsen(List<String> lines) {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (int value = 0; value < lines.size(); value++) {
+			String line = lines.get(value);
+			for (int item = 0; item < line.length(); item++) {
+				String temp = lines.get(item);
+				Integer tempVal = map.get(temp);
+				if (tempVal != null) {
+					map.put(temp, new Integer(tempVal + 1));
+				} else {
+					tempVal = 1;
+				}
+			}
+		}
+	}
+
+	public String formatMostToLeast(List<String> lines) {
+	}
+
+	public String tenLFreqTokenInsen() {
 
 	}
 
